cabf5851d64e0ee3c18b8f95079d9a83fcecdbfe
SHIMOMURA Sho <graueule@gmail.com>
2014-12-29 17:43:49 +0900

    basic expression evaluator

--- Interpreter/main.cpp
+++ Interpreter/main.cpp
@@ -7,26 +7,36 @@
 
 struct ParserNum : Parser<int> {
     int parse(Input &input) {
-        return Many1(Digit())->parse(input).toInt();
+        int result = Many1(Digit())->parse(input).toInt();
+        return setOut(result);
     }
 };
+
+typedef int(*intoperator)(int, int);
+struct ParserPlus : Parser<int(*)(int,int)> {
+    int (*parse(Input &input))(int, int) {
+        Char('+')->parse(input);
+        return [](int x, int y){ return x + y; };
+    }
+};
+
 struct ParserTerm : Parser<int> {
     int parse(Input &input) {
-        QList<int> nums = Between( SepBy1(
-                                Choice({ new ParserNum(), new ParserTerm() }),
-                                Many1(Space())),
+        auto sum = Between( Chainl( Choice({ new ParserNum(), new ParserTerm() }),
+                                    Between(new ParserPlus(), Many(Space()), Many(Space())),
+                                    0),
                             Char('('),
                             Char(')'))->parse(input);
-        int result = 0;
-        Q_FOREACH(int x, nums)
-            result += x;
-        return setOut(result);
+        return setOut(sum);
     }
 };
 
 int main(int argc, char *argv[])
 {
-    Input input("(12 34 (56 78) 10 (20))");
+    Q_UNUSED(argc);
+    Q_UNUSED(argv);
+
+    Input input("(12 + 34 + (56 + 78) + 10 + (20))");
     auto sum = S(new ParserTerm())->parse(input);
     qDebug() << sum;
 
